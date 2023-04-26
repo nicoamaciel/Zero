2ab734b6cd951205c40b0ae772d16dd046ae6f6b
unknown <nicoamaciel@outlook.com>
2023-04-26 20:57:14 -0300

    20:57 26/04

---
+++ proyectoZero/proyectoZero/Logica/Lcargos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace proyectoZero.Logica
+{
+    public class Lcargos
+    {
+        public int ID_Cargo { get; set; }
+        public string Cargo { get; set; }
+        public double SueldoPorHora { get; set; }
+        
+    }
+}
